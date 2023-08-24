0167c6fac27e4cfdb7a865308562d5b037fb8f98
João Pedro de Oliveira Teles <80984493+joaopedroteles@users.noreply.github.com>
2023-08-24 19:40:15 -0300

    Add files via upload

---
+++ Jxadrez/src/com/chess/engine/pieces/Piece.java
@@ -0,0 +1,128 @@
+package com.chess.engine.pieces;
+
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.Collection;
+import java.util.List;
+
+public abstract class Piece {
+
+    protected final PieceType pieceType;
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
+    private final int cachedHashCode;
+
+    Piece(final PieceType pieceType,
+          final  int piecePosition,
+          final Alliance pieceAlliance) {
+        this.pieceType = pieceType;
+        this.pieceAlliance = pieceAlliance;
+        this.piecePosition = piecePosition;
+        //TODO more work here
+        this.isFirstMove = false;
+        this.cachedHashCode = computeHashCode();
+    }
+
+    private int computeHashCode() {
+        int result =pieceType.hashCode();
+        result = 31 * result + pieceAlliance.hashCode();
+        result = 31 * result + piecePosition;
+        result = 31 * result + (isFirstMove ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        if(this == other){
+            return true;
+        }
+        if(!(other instanceof Piece)){
+            return false;
+        }
+        final Piece otherPiece = (Piece) other;
+        return piecePosition == otherPiece.getPiecePosition() && pieceType == otherPiece.getPieceType() &&
+                pieceAlliance == otherPiece.getPieceAlliance() && isFirstMove == otherPiece.isFirstMove();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.cachedHashCode;
+    }
+
+    public int getPiecePosition() {
+        return this.piecePosition;
+    }
+
+    public Alliance getPieceAlliance() {
+        return this.pieceAlliance;
+    }
+
+    public boolean isFirstMove(){
+        return this.isFirstMove;
+    }
+
+    public PieceType getPieceType() {
+        return this.pieceType;
+    }
+
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
+
+    public abstract Piece movePiece(Move move);
+
+    public enum PieceType {
+
+        PAWN("p") {
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        BISHOP("B") {
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        KING("K") {
+            @Override
+            public boolean isKing() {
+                return true;
+            }
+        },
+        QUEEN("Q") {
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        KNIGHT("K") {
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        ROOK("R") {
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        };
+
+        private String pieceName;
+
+        PieceType(final String pieceName) {
+            this.pieceName = pieceName;
+        }
+
+        public String toString() {
+            return this.pieceName;
+        }
+
+        public abstract boolean isKing();
+    }
+
+}
